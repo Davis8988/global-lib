58e4be2d81ee318058cfb5782243bc48b437743b
davis8988 <Davismarsel@gmail.com>
2019-11-30 21:59:22 +0200

    Added print to JsonOutput.prettyPrint("${remoteJenkinsJobStatus}")

--- vars/TriggerRemoteJenkinsJobWithParameters.groovy
+++ vars/TriggerRemoteJenkinsJobWithParameters.groovy
@@ -113,7 +113,7 @@ def waitForRemoteJenkinsJobToFinish(jobUrl, nextBuildNumber, timeoutSeconds, sle
 	}
 	
 	print "Done waiting for remote job to finished building.."
-	JsonOutput.prettyPrint("${remoteJenkinsJobStatus}")
+	print JsonOutput.prettyPrint("${remoteJenkinsJobStatus}")
 	return remoteJenkinsJobStatus_Json
 }
 
