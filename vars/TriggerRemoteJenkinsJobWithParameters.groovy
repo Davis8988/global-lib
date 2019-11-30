23fa9af8151104ffb3b035d2c309c891c45e5760
davis8988 <Davismarsel@gmail.com>
2019-11-30 12:35:36 +0200

    Added tests dir to gitignore

--- vars/TriggerRemoteJenkinsJobWithParameters.groovy
+++ vars/TriggerRemoteJenkinsJobWithParameters.groovy
@@ -10,22 +10,34 @@ def jsonParse(def json) {
 
 
 def call(Map[:] args) {
-	remoteJenkinsJobUrl_org = args.jobUrl ? null
-	remoteJenkinsJobToken = args.token ? null
+	arg_remoteJenkinsJobUrl = args.jobUrl ? null
+	arg_remoteJenkinsJobToken = args.token ? null
 	
-	/* Check mandatory params */
-	if (! remoteJenkinsJobUrl_org || !remoteJenkinsJobToken) {
-		error "Missing mandatory params: \n" +
-			  "jobUrl=${remoteJenkinsJobUrl_org} \n" +
-			  "token=${remoteJenkinsJobToken} \n"
+	/* Check mandatory args */
+	if (! arg_remoteJenkinsJobUrl || ! arg_remoteJenkinsJobToken) {
+		error "Missing mandatory args: \n" +
+			  "jobUrl=${arg_remoteJenkinsJobUrl} \n" +
+			  "token=${arg_remoteJenkinsJobToken} \n"
 	}
 	
 	println "Received args: \n" +
-			"FName=${FName} \n" +
-			"LName=${LName} \n" 
+			"jobUrl=${arg_remoteJenkinsJobUrl} \n" +
+			"token=${arg_remoteJenkinsJobToken} \n" 
 			
-	def remoteJenkinsJobUrl_Corrected = getCorrectRemoteJenkinsJobUrl()
+	def remoteJenkinsJobUrl_Corrected = getCorrectRemoteJenkinsJobUrl(arg_remoteJenkinsJobUrl)
+	def remoteJenkins_Status = getRemoteJenkinsStatus()
 	def params = jsonParse(env.choice_app)
+}
+
+
+def getCorrectRemoteJenkinsJobUrl(arg_remoteJenkinsJobUrl) {
+	remoteJenkinsJobUrl_Corrected = arg_remoteJenkinsJobUrl.trim().replaceAll("/buildWithParameters" , "")
+	remoteJenkinsJobUrl_Corrected = remoteJenkinsJobUrl_Corrected.replaceAll("/build" , "")
+	
+	def strLength = remoteJenkinsJobUrl_Corrected.length()
+	if (remoteJenkinsJobUrl_Corrected[strLength-1] == '/') {remoteJenkinsJobUrl_Corrected=remoteJenkinsJobUrl_Corrected.substring(0, strLength-1)}
+	
+	return remoteJenkinsJobUrl_Corrected
 }
 
 
