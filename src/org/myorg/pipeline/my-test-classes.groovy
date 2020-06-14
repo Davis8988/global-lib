43fbef8eb0985bb9365aff35c2a0d06d36773103
= <=>
2020-06-14 10:03:09 +0300

    Added shared lib src files

---
+++ src/org/myorg/pipeline/my-test-classes.groovy
@@ -0,0 +1,17 @@
+class Example {
+   static void main(String[] args) {
+      Student mst = new Student();
+      mst.name = "Joe";
+      mst.ID = 1;
+      println(mst.Display())
+   } 
+} 
+ 
+public class Student {
+   String name;
+   int ID;
+	
+   String Display() {
+      return name +ID;
+   }  
+}
