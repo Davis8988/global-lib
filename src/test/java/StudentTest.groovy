b9ad5f1791a28464a01ae62726c652b4e1118141
= <=>
2020-06-14 12:00:24 +0300

    Added printings

--- src/test/java/StudentTest.groovy
+++ src/test/java/StudentTest.groovy
@@ -4,6 +4,7 @@ class StudentTest extends GroovyTestCase {
    void testDisplay() {
       def stud = new Student(name : 'Joe', ID : '1')
       def expected = 'Joe11'
+	  print "Testing expected output"
       assertToString(stud.Display(), expected)
    }
 }
