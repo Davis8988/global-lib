4b7c78afdf7aa0731b775e3890551465d55fccf9
= <=>
2020-06-14 11:59:28 +0300

    Testing failure on purpose of test

--- src/test/java/StudentTest.groovy
+++ src/test/java/StudentTest.groovy
@@ -3,7 +3,7 @@ import org.myorg.pipeline.Student
 class StudentTest extends GroovyTestCase {
    void testDisplay() {
       def stud = new Student(name : 'Joe', ID : '1')
-      def expected = 'Joe1'
+      def expected = 'Joe11'
       assertToString(stud.Display(), expected)
    }
 }
