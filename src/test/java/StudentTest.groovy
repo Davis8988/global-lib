99cf15f1fcfa80f7b3f5e97c5da98cbcab0bc9fa
= <=>
2020-06-14 12:48:36 +0300

    Added printing and another test

--- src/test/java/StudentTest.groovy
+++ src/test/java/StudentTest.groovy
@@ -1,12 +1,26 @@
 import org.junit.Test
 import org.myorg.pipeline.Student
 
+import static groovy.test.GroovyAssert.shouldFail
+
+
 class StudentTest extends GroovyTestCase {
    @Test
    void testDisplay() {
-      def stud = new Student(name : 'Joe', ID : '1')
-      def expected = 'Joe11'
-	  print "Testing expected output"
-      assertToString(stud.Display(), expected)
+		print "Testing display()"
+		def stud = new Student(name : 'Joe', ID : '1')
+		def expected = 'Joe11'
+		print "Testing expected output"
+		assertToString(stud.Display(), expected)
    }
+   
+   @Test
+   void indexOutOfBoundsAccess() {
+	   print "Testing indexOutOfBoundsAccess()"
+       def numbers = [1,2,3,4]
+       shouldFail {
+           numbers.get(4)
+       }
+   }
+   
 }
